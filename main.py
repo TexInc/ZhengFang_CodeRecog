9b02c38ed3771841df5eb8a1792de7ba439f0dca
Loyio Hex <i@loyio.me>
2024-01-28 08:56:12 +0800

    update something

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from predict_func import verify
 
 
 if __name__ == '__main__':
-    the_url = 'http://125.221.35.100/CheckCode.aspx'
+    the_url = 'http://127.0.0.1/CheckCode.aspx'
     the_model = 'model/SVC_Model_zf.pkl'
     captcha_list = list(verify(the_url, the_model))
     captcha_code = ''.join(captcha_list)
